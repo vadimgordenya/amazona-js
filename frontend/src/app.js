d9720ab105d834cd507c29b91d1fb3817083aed5
Vadzim_Hardzenia <vadimgordenya@gmail.com>
2020-12-19 12:36:57 +0300

    Build url router

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,24 @@
 import HomeScreen from './srceens/HomeScreen.js';
-const router = () => {
-  const main = document.getElementById('main-container');
-  main.innerHTML = HomeScreen.render();
+import ProductScreen from './srceens/ProductScreen.js';
+import Error404Screen from './srceens/Error404Screen.js';
+
+import { parseRequestUrl } from "./utils.js";
+
+const routes = {
+  '/': HomeScreen,
+  '/product/:id': ProductScreen
 };
+
+const router = () => {
+  const request = parseRequestUrl();
+  const parseUrl =
+    (request.resource ? `/${request.resource}` : '/') +
+    (request.id ? '/:id' : '') +
+    (request.action ? `${request.action}` : '');
+  const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
+  const main = document.getElementById('main-container');
+  main.innerHTML = screen.render();
+};
+
 window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
